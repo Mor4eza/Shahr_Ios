d515b383d7730f282e2bce9668ad8b88902a084b
zigool <zigoool2013@gmail.com>
2015-10-06 09:29:01 +0330

    Initial Commit

---
+++ ShahrMa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShahrMa
+//
+//  Created by Morteza on 10/6/15.
+//  Copyright (c) 2015 Ariana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
